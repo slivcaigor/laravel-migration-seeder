3aed32b5bac63668d4123bd5dbaf57cda2a50436
slivcaigor <slivcaigor@gmail.com>
2023-02-01 13:26:10 +0100

    travel migration pushed to database

--- database/migrations/2023_02_01_115022_travel.php
+++ database/migrations/2023_02_01_115022_travel.php
@@ -13,7 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('package_name');
+            $table->string('destination');
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->decimal('price', 8, 2);
+            $table->text('description');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('travels');
     }
 };
